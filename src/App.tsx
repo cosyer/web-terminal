c979acd5761a2cfc17d205a9c7d464836c605e45
cosyer <chenyu@mydearest.cn>
2021-01-19 00:38:06 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import WebTerminal from "./Terminal";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <WebTerminal />
+    </div>
+  );
+}
+
+export default App;
